82ae630ccdc43bcad0e110552c8b06f0fae57784
Jonathan Digay <jonthan.digay1@gmail.com>
2022-03-31 17:00:19 +0800

    last commit

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 // header actions
 const menu = document.querySelector(".menu");
 const navs = document.querySelector(".navs");
+const nav = document.querySelectorAll(".nav");
 const mode = document.querySelector(".mode");
 const overlay = document.querySelector(".overlay");
+
 menu.addEventListener("click", () => {
   navs.classList.replace("navs-off", "navs-on");
 });
 overlay.addEventListener("click", () => {
   navs.classList.replace("navs-on", "navs-off");
 });
+
+nav.forEach((current) => {
+  current.addEventListener("click", () => {
+    navs.classList.replace("navs-on", "navs-off");
+  });
+});
+
 mode.addEventListener("click", () => {
-  const Class = document.body.className;
-  !Class
-    ? document.body.classList.add("darkmode")
-    : document.body.classList.remove("darkmode");
+  const darmode = document.body.className;
+  !darmode
+    ? (document.body.classList.add("darkmode"),
+      (mode.innerHTML = ` <i class="material-icons">sunny</i>`))
+    : (document.body.classList.remove("darkmode"),
+      (mode.innerHTML = ` <i class="material-icons">nightlight</i>`));
 });
